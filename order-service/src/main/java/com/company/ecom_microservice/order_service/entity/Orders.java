be4cc680f4ca556f5ce1f175b8e22524fcab2fe6
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-07-14 10:37:00 +0530

    added the OrderItem and Orders entity

--- order-service/src/main/java/com/company/ecom_microservice/order_service/entity/Orders.java
+++ order-service/src/main/java/com/company/ecom_microservice/order_service/entity/Orders.java
@@ -1,4 +1,29 @@
 package com.company.ecom_microservice.order_service.entity;
 
+
+import com.company.ecom_microservice.order_service.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
 public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    private Double price;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> items;
 }
---
+++ order-service/src/main/java/com/company/ecom_microservice/order_service/enums/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.company.ecom_microservice.order_service.enums;
+
+public enum OrderStatus {
+
+    CONFIRMED, CANCELLED, PENDING
+}
