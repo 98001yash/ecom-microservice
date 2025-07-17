d1dcd5da332ed67de65c57879c3d2b762cb0c2a9
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-07-17 17:40:30 +0530

    added some more configuration in the globalLogging

--- api-gateway/src/main/java/com/company/ecom_microservice/api_gateway/filters/GlobalLoggingFilter.java
+++ api-gateway/src/main/java/com/company/ecom_microservice/api_gateway/filters/GlobalLoggingFilter.java
@@ -3,6 +3,7 @@ package com.company.ecom_microservice.api_gateway.filters;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -10,13 +11,20 @@ import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
-public class GlobalLoggingFilter implements GlobalFilter {
+public class GlobalLoggingFilter implements GlobalFilter, Ordered {
 
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 
         log.info("Logging from Global: {}",exchange.getRequest().getURI());
-        return chain.filter(exchange);
+        return chain.filter(exchange).then(Mono.fromRunnable(()->{
+            log.info("Logging from global Post: {}",exchange.getResponse().getStatusCode());
+        }));
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
     }
 }
