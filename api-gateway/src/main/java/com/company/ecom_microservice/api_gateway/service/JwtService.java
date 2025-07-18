3d85c192ba0dc7b036508786e85cc8ceadba7508
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-07-18 07:39:41 +0530

    added the JwtService in the api-gateway and also added the  method get userId from Token

---
+++ api-gateway/src/main/java/com/company/ecom_microservice/api_gateway/service/JwtService.java
@@ -0,0 +1,26 @@
+package com.company.ecom_microservice.api_gateway.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+public class JwtService {
+
+    public String jwtSecretKey;
+
+    private SecretKey getSecretKey(){
+        return Keys.hmacShaKeyFor(jwtSecretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public Long getUserIdFromToken(String token){
+        Claims claims = Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return Long.valueOf(claims.getSubject());
+    }
+}
