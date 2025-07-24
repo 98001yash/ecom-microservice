2eca251f223f0254653f54250d0843c188a032fd
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-07-24 17:31:43 +0530

    added the config-server in the ecom-microrservice

---
+++ config-server/src/test/java/com/company/ecom_microservice/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.ecom_microservice.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
